b04981d9ecb8f2997b8928860eae6e1340049a69
noran <nora200336@gmail.com>
2023-08-21 17:31:57 -0700

    f

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -2,10 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <unistd.h>
 
-int _putchar(char c);
-void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+void reset_to_98(int *n);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
@@ -16,4 +16,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif /* MAIN_H */
+#endif
