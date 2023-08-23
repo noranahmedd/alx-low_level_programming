da5e4145476526481e8ad332eeb6788ea7cbe303
noran <nora200336@gmail.com>
2023-08-23 14:36:50 -0700

    solved

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,10 +17,10 @@ int main(void)
 			putchar(i + '0');
 			putchar(j + '0');
 			if (i < 8 || j < 9)
-                        {	
+			{	
 				putchar(',');
 				putchar(' ');
-                        }
+			}
 		}
 	}
 	putchar('\n');
