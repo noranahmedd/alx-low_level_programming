af26b2dbe704e4e7a28a056c13f9c7cce995c0b1
noran <nora200336@gmail.com>
2023-08-25 12:24:12 -0700

    g

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,20 +8,19 @@
  */
 char *leet(char *c)
 {
-	char *cc =c;
+	char *cc = c;
 	char key [] = {'a', 'e', 'o', 't', 'l'};
 	int value[] = {4, 3, 0, 7, 1};
 	unsigned int i;
 
-	while (*c) 
+	while (*c)
 	{
 		for (i = 0; i < sizeof(key) / sizeof(char); i++)
 		{
-			if (*c == key[i] || *c == key [i] - 32)
-				*c = 48 + value[i];
+		if (*c == key[i] || *c == key[i] - 32)
+			*c = 48 + value[i];
 		}
 		c++;
 	}
-	
 	return (cc);
 }
