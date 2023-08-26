b0fca319b7fa5707d6c089886d244c971ec7b7cf
noran <nora200336@gmail.com>
2023-08-25 19:35:41 -0700

    f

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,3 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer using _putchar
+ * @n: The integer to be printed
+ */
 void print_number(int n)
 {
+	int divisior = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	while (n / divisor >= 10)
+		divisor *= 10;
+
+	while (divisor)
+	{
+		_putchar(n / divisor + '0');
+		n %= divisor;
+		divisor /= 10;
+	}
 }
