b714e57d87f56691747ba30843680c827f81bade
noran <nora200336@gmail.com>
2023-08-25 20:08:38 -0700

    t

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,2 +1,53 @@
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-:
+#include "main.h"
+
+/**
+ * infinite_add - Adds two numbers
+ * @n1: The first number
+ * @n2: The second number
+ * @r: The buffer to store the result
+ * @size_r: The buffer size
+ *
+ * Return: A pointer to the result, or 0 if result can't be stored in r
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int len1 = 0, len2 = 0, carry = 0, sum = 0;
+	int i, j, k;
+
+	while (n1[len1] != '\0')
+		len1++;
+	while (n2[len2] != '\0')
+		len2++;
+
+	if (len1 + 1 > size_r || len2 + 1 > size_r)
+		return (0);
+
+	i = len1 - 1;
+	j = len2 - 1;
+	k = 0;
+
+	while (i >= 0 || j >= 0 || carry > 0)
+	{
+		int digit1 = (i >= 0) ? n1[i] - '0' : 0;
+		int digit2 = (j >= 0) ? n2[j] - '0' : 0;
+
+		sum = digit1 + digit2 + carry;
+		carry = sum / 10;
+		r[k] = (sum % 10) + '0';
+
+		i--;
+		j--;
+		k++;
+	}
+
+	r[k] = '\0';
+
+	for (i = 0, j = k - 1; i < j; i++, j--)
+	{
+		char temp = r[i];
+		r[i] = r[j];
+		r[j] = temp;
+	}
+
+	return (r);
+}
