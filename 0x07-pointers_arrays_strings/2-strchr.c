07eb98789679c5deb2f2954f90c2b760147e6fc5
noran <nora200336@gmail.com>
2023-08-28 15:04:13 -0700

    done till 4

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strchr - Locates a character in a string.
+ * @s: Pointer to the string to search.
+ * @c: Character to locate.
+ *
+ * Return: Pointer to the first occurrence of character c in the string s,
+ *         or NULL if character is not found.
+ */
+char *_strchr(char *s, char c)
+{
+
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+
+	return (NULL);
+}
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strpbrk - Searches a string for any of a set of bytes.
+ * @s: Pointer to the string to search.
+ * @accept: Pointer to the set of bytes to search for.
+ *
+ * Return: Pointer to the byte in s that matches one of the bytes in accept,
+ *         or NULL if no such byte is found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+				return (&s[i]);
+		}
+	}
+
+	return (NULL);
+}
