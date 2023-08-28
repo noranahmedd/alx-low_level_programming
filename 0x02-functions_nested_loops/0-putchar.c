bce6f64a0f9a6fd6f38f116b8d3a445442d974f1
noran <nora200336@gmail.com>
2023-08-27 22:09:16 -0700

    f

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,11 +9,11 @@ int main(void)
 	char str[] = "_putchar";
 	int i;
 
-	for (i = 0; str[i] != "\0"; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
-	_putchar("\n");
+	_putchar('\n');
 
 	return (0);
 }
