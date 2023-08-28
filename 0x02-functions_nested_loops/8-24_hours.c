5e1496ec47506f875ca02aee34aa498f81a14751
noran <nora200336@gmail.com>
2023-08-27 22:06:38 -0700

    f

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,7 +9,7 @@ void jack_bauer(void)
 
 	for (hours = 0; hours <= 24; hours++)
 	{
-		for (minutes = 0; minutes <= 60; minutes++)
+		for (minutes = 0; minutes < 60; minutes++)
 		{
 			_putchar(hours / 10 + '0');
 			_putchar(hours % 10 + '0');
