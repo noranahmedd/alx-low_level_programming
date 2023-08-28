b47ff13697ecae37ee040699e09d567e6e64659e
noran <nora200336@gmail.com>
2023-08-28 16:48:14 -0700

    f

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,9 +15,9 @@ void print_diagsums(int *a, int size)
 		for (j = 0; j < size ; j++)
 		{
 			if (i == j)
-				sum1 += a[i][j];
+				sum1 += a[i * size + j];
 			if ((i + j) == (size - 1))
-				sum2 += a[i][j];
+				sum2 += a[i * size + j];
 		}
 	}
 	printf("%d, %d", sum1, sum2);
