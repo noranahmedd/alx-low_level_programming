5eb0ced35740f2b2109386a38742d0e78713e3a8
noran <nora200336@gmail.com>
2023-09-18 19:21:47 -0700

    g

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,10 +7,8 @@
 *
 * Return: Always 0
 */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attritbute__((unused)))
 {
-	(void)argv[];
-
 	printf("%d\n", argc - 1);
 
 	return (0);
