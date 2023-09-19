bfaee93e403ca370ee755856c1b960c11dbfcbbc
noran <nora200336@gmail.com>
2023-09-18 18:17:25 -0700

    fixes

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -17,6 +17,6 @@ char *_strchr(char *s, char c)
 		if (s[i] == c)
 			return (s + i);
 		if (s[i] == '\0')
-			return (NULL);
+			return (0);
 	}
 }
--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -30,5 +30,5 @@ char *_strstr(char *haystack, char *needle)
 		haystack++;
 	}
 
-	return (NULL);
+	return (0);
 }
