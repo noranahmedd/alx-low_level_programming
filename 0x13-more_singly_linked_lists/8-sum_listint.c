f7542453d0ab3d2574c1a96f24b21fe0cf29b945
noran <nora200336@gmail.com>
2023-09-26 15:11:08 -0700

    n

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,16 +1,18 @@
 #include "lists.h"
 
 /**
- * sum_listint - Computes the sum of all the data (n) in a listint_t linked list.
+ * sum_listint - Computes the sum of all
+ * the data (n) in a listint_t linked list.
  * @head: Pointer to the head node of the list.
  *
- * Return: The sum of all the data in the linked list. If the list is empty, return 0.
+ * Return: The sum of all the data in the
+ * linked list. If the list is empty, return 0.
  */
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
 	listint_t *temp = head;
-	
+
 	if (!head)
 		return (0);
 
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,19 @@
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	int i = 0;
+	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *temp = *head;
+	new_node->n = n;
+
+	while (*temp)
+	{
+		if (i == index)
+		{
+			new_node->next = temp->next;
+			temp->next = new_node;
+			return
+		}
+
+		       	
+	}
+}
