ed84a5d42c42bc0a4cb19df76bf651f1423aca46
noran <nora200336@gmail.com>
2023-09-26 09:23:25 -0700

    f

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,17 +6,17 @@
  */
 size_t print_list(const list_t *h)
 {
+	size_t count = 0;
+	
 	while (h != NULL)
 	{
-		size_t num = 0;
-
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		num++;
+		count++;
 
 	}
-	return (num);
+	return (count);
 }
