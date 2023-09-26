f445aaf6a710e4e38d04428963c0d7e12c58d51c
noran <nora200336@gmail.com>
2023-09-26 09:03:07 -0700

    fix

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include<stdio.h>
-
+#include<stdlib.h>
+#include<string.h>
 int _putchar(char c);
 
 /**
@@ -19,10 +20,11 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
+int _strlen(char *s);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /* MAIN_H */
+#endif /* LISTS_H */
