d230dbcb2a4f425297bf5cb40d481342dc7c282a
noran <nora200336@gmail.com>
2023-11-04 03:32:16 -0700

    v

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,22 +7,21 @@
  */
 char *_strdup(char *str)
 {
+	int len = 0, i;
+	char *duplicate;
+
 	if (str == NULL) {
 		return (NULL);
 	}
 
-	int len = 0;
-
 	while (str[len] != '\0') {
 		len++;
 	}
-
-	char *duplicate = (char *)malloc((len + 1) * sizeof(char));
+	duplicate = malloc(len + 1);
 
 	if (duplicate == NULL) {
 		return (NULL);
 	}
-	int i;
 
 	for ( i = 0; i <= len; i++) {
 		duplicate[i] = str[i];
